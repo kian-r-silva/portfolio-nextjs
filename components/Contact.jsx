02d9e4a09d327fbcbb82731baec987cf2fc89fbc
kian-silva <118226162+kian-silva@users.noreply.github.com>
2024-08-24 11:44:33 -0400

    contact form

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import contactImg from '../public/assets/contact.jpg'
 import Image from 'next/image'
 import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
+import Link from 'next/link';
+import { HiOutlineChevronDoubleUp } from 'react-icons/hi';
 
 
 const Contact = () => {
@@ -44,8 +46,46 @@ const Contact = () => {
                     </div>
                 </div>
 {/* right */}
-            
+            <div className='col-span-3 w-full h-auto rounded-xl shadow-xl shadow-gray-400 lg:p-4'>
+                <div className='p4'>
+                    <form>
+                        <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
+                            <div className='flex flex-col'>
+                                <label className='uppercase text-small'>Name</label>
+                                <input type='text' className='border-2 rounded-lg p-3 flex border-gray-200'/>
+                            </div>
+                            <div className='flex flex-col'>
+                                <label className='uppercase text-small'>Phone Number</label>
+                                <input type='text' className='border-2 rounded-lg p-3 flex border-gray-200'/>
+                            </div>
+                        </div>
+                        <div className='flex flex-col py-2'>
+                                <label className='uppercase text-small'>Email</label>
+                                <input type='email' className='border-2 rounded-lg p-3 flex border-gray-200'/>
+                        </div>
+                        <div className='flex flex-col py-2'>
+                                <label className='uppercase text-small'>Subject</label>
+                                <input type='text' className='border-2 rounded-lg p-3 flex border-gray-200'/>
+                        </div>
+                        <div className='flex flex-col py-2'>
+                                <label className='uppercase text-small'>Message</label>
+                                <textarea className='border-2 rounded-lg p3 border-gray-200' rows='10'></textarea>
+                        </div>
+                        <button className='w-full p-4 text-gray-100 mt-4'>Send Message</button>
+                    </form>
+                </div>
             </div>
+            </div>
+            <div className='flex justify-center py-12'>
+                <Link href='/'>
+                    <div className='rounded-full shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                        <HiOutlineChevronDoubleUp
+                        className='text-[#5651e5]'
+                        size={30}
+                        />
+                    </div>
+                </Link>
+            </div> 
         </div>
     </div>
   )
