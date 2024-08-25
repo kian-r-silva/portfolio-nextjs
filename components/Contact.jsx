a8c8a7d11d269ad728659209ad6273b36a4b6a19
kian-silva <118226162+kian-silva@users.noreply.github.com>
2024-08-25 14:59:09 -0400

    contact form complete

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -7,7 +7,7 @@ import { HiOutlineChevronDoubleUp } from 'react-icons/hi';
 
 const Contact = () => {
   return (
-    <div id='contact' className='w-full h-screen bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 text-gray-300 lg:h-screen'>
+    <div id='contact' className='w-full min-h-screen bg-gradient-to-r from-gray-900 via-gray-800 to-gray-900 text-gray-300 lg:min-h-screen'>
       <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
         <p className='text-xl text-[#e94560] tracking-widest uppercase'>Contact</p>
         <h2 className='py-4'>Get In Touch</h2>
@@ -31,46 +31,52 @@ const Contact = () => {
               <div>
                 <p className='uppercase pt-8'>Connect With Me!</p>
                 <div className='flex items-center justify-between py-4'>
-                  <div className='rounded-full shadow-lg  shadow-gray-900 p-3 cursor-pointer hover:scale-110 ease-in duration-300'>
-                    <FaLinkedin className='w-7 h-7' />
-                  </div>
-                  <div className='rounded-full shadow-lg  shadow-gray-900 p-3 cursor-pointer hover:scale-110 ease-in duration-300'>
-                    <FaGithub className='w-7 h-7' />
-                  </div>
-                  <div className='rounded-full shadow-lg  shadow-gray-900 p-3 cursor-pointer hover:scale-110 ease-in duration-300'>
-                    <FaInstagram className='w-7 h-7' />
-                  </div>
+                  <a href='https://www.linkedin.com/in/kian-silva-/' target='_blank' rel='noopener noreferrer'>
+                    <div className='rounded-full shadow-lg shadow-gray-900 bg-gray-700 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                      <FaLinkedin className='w-8 h-8 text-[#ffffff]' />
+                    </div>
+                  </a>
+                  <a href='https://github.com/kian-r-silva' target='_blank' rel='noopener noreferrer'>
+                    <div className='rounded-full shadow-lg shadow-gray-900 bg-gray-700 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                      <FaGithub className='w-8 h-8 text-[#ffffff]' />
+                    </div>
+                  </a>
+                  <a href='https://www.instagram.com/kian.silva/' target='_blank' rel='noopener noreferrer'>
+                    <div className='rounded-full shadow-lg shadow-gray-900 bg-gray-700 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+                      <FaInstagram className='w-8 h-8 text-[#ffffff]' />
+                    </div>
+                  </a>
                 </div>
               </div>
             </div>
           </div>
           <div className='col-span-3 w-full h-auto rounded-xl shadow-xl shadow-gray-900 bg-gray-600 lg:p-4'>
             <div className='p-4'>
-              <form>
+              <form method='POST' action='https://getform.io/f/bnlewpnb '>
                 <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
-                  <div className='flex flex-col'>
+                  <div className='flex flex-col py-2'>
                     <label className='uppercase text-small'>Name</label>
-                    <input type='text' className='border-2 rounded-lg p-3 flex border-gray-200' />
+                    <input type='text' name='name' className='border-2 rounded-lg p-3 flex border-gray-200' />
                   </div>
-                  <div className='flex flex-col'>
+                  <div className='flex flex-col py-2'>
                     <label className='uppercase text-small'>Phone Number</label>
-                    <input type='text' className='border-2 rounded-lg p-3 flex border-gray-200' />
+                    <input type='tel' name='phone' className='border-2 rounded-lg p-3 flex border-gray-200' />
                   </div>
                 </div>
                 <div className='flex flex-col py-2'>
                   <label className='uppercase text-small'>Email</label>
-                  <input type='email' className='border-2 rounded-lg p-3 flex border-gray-200' />
+                  <input type='email' name='email' className='border-2 rounded-lg p-3 flex border-gray-200' />
                 </div>
                 <div className='flex flex-col py-2'>
                   <label className='uppercase text-small'>Subject</label>
-                  <input type='text' className='border-2 rounded-lg p-3 flex border-gray-200' />
+                  <input type='text' name='subject' className='border-2 rounded-lg p-3 flex border-gray-200' />
                 </div>
                 <div className='flex flex-col py-2'>
                   <label className='uppercase text-small'>Message</label>
-                  <textarea className='border-2 rounded-lg p-3 border-gray-200' rows='10'></textarea>
+                  <textarea type='text' name='message' className='border-2 rounded-lg p-3 border-gray-200' rows='10' />
                 </div>
-                <button className='w-full p-4 text-white bg-[#e94560] mt-4 rounded-lg hover:bg-[#d22c4d] transition duration-300 ease-in-out'>
-                    Send Message
+                <button type='submit' className='w-full p-4 text-white bg-[#e94560] mt-4 rounded-lg hover:bg-[#d22c4d] transition duration-300 ease-in-out'>
+                  Send Message
                 </button>
 
               </form>
