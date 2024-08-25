df3e8f352d42dc0250a5150c105fdac011eca582
kian-silva <118226162+kian-silva@users.noreply.github.com>
2024-08-25 11:29:50 -0400

    NavBar Update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,37 +1,43 @@
-"use client"
-import React, { useEffect, useState } from 'react';
+"use client";
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { RiCloseFill, RiMenuFill } from "react-icons/ri";
 import { FaLinkedin, FaGithub, FaInstagram } from "react-icons/fa";
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 const Navbar = () => {
     const [nav, setNav] = useState(false);
     const [shadow, setShadow] = useState(false);
+    const [navBg, setNavBg] = useState('#ecf0f3');
+    const [linkColor, setLinkColor] = useState('#1f2937');
 
     const handleNav = () => {
         setNav(!nav);
     }
 
-    useEffect(() =>  {
+    useEffect(() => {
         const handleShadow = () => {
-            if (window.scrollY >= 90){
-                setShadow(true)
+            if (window.scrollY >= 90) {
+                setShadow(true);
             } else {
-                setShadow(false)
+                setShadow(false);
             }
         }
-        window.addEventListener('scroll', handleShadow)
-    })
+        window.addEventListener('scroll', handleShadow);
+        return () => {
+            window.removeEventListener('scroll', handleShadow);
+        };
+    }, []);
 
     return (
-        <div className={shadow? 'fixed w-full h-20 shadow-xl z-[100]': 'fixed w-full h-20 z-[100]'}>
+        <div style={{ backgroundColor: `${navBg}` }} className={`${shadow ? 'shadow-xl' : ''} ${navBg} fixed w-full h-20 z-[100]`}>
             <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
                 <Link href='/#home'>
-                <Image src="/assets/KianSilvaIcon.png" alt="KianSilvaIcon" width={125} height={50} />
+                    <Image src="/assets/KianSilvaIcon.png" alt="KianSilvaIcon" width={125} height={50} />
                 </Link>
                 <div>
-                    <ul className='hidden md:flex'>
+                    <ul style={{ color: `${linkColor}` }} className='hidden md:flex'>
                         <Link href='/#home'>
                             <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
                         </Link>
@@ -48,21 +54,21 @@ const Navbar = () => {
                             <li className='ml-10 text-sm uppercase hover:border-b'>Contact</li>
                         </Link>
                     </ul>
-                    <div onClick={handleNav} className='md:hidden'>
-                        <RiMenuFill size={25}/>
+                    <div style={{ color: `${linkColor}` }} onClick={handleNav} className='md:hidden'>
+                        <RiMenuFill size={25} />
                     </div>
                 </div>
             </div>
 
             <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/30' : ''}>
-                <div className={nav?'md:hidden fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#addbff] p-10 ease-in duration-500'
-                                : 'fixed left-[-100%] p-10 ease-in duration-500'}>
+                <div className={nav ? 'md:hidden fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#addbff] p-10 ease-in duration-500'
+                    : 'fixed left-[-100%] p-10 ease-in duration-500'}>
                     <div className='flex w-full items-center justify-between'>
                         <Link href='/'>
-                        <Image src="/assets/KianSilvaIcon.png" alt="KianSilvaIcon" width={100} height={50} />                            
+                            <Image src="/assets/KianSilvaIcon.png" alt="KianSilvaIcon" width={100} height={50} />
                         </Link>
                         <div onClick={handleNav} className='p-3 cursor-pointer'>
-                            <RiCloseFill size={20}/>
+                            <RiCloseFill size={20} />
                         </div>
                     </div>
                     <div className='border-b border-gray-300 my-4'>
@@ -79,11 +85,11 @@ const Navbar = () => {
                             <Link href='/#skills'>
                                 <li onClick={() => setNav(false)} className='py-4 text-sm'>Skills</li>
                             </Link>
-                            <Link href='/#rojects'>
+                            <Link href='/#projects'>
                                 <li onClick={() => setNav(false)} className='py-4 text-sm'>Projects</li>
                             </Link>
-                            <Link onClick={() => setNav(false)} href='/#contact'>
-                                <li className='py-4 text-sm'>Contact</li>
+                            <Link href='/#contact'>
+                                <li onClick={() => setNav(false)} className='py-4 text-sm'>Contact</li>
                             </Link>
                         </ul>
                         <div className='pt-40'>
@@ -93,13 +99,13 @@ const Navbar = () => {
                                     <FaLinkedin className='w-7 h-7' />
                                 </div>
                                 <div className='rounded-md shadow-lg shadow-gray-500 p-3 cursor-pointer hover:scale-105'>
-                                    <FaGithub className='w-7 h-7'/>
+                                    <FaGithub className='w-7 h-7' />
                                 </div>
                                 <div className='rounded-md shadow-lg shadow-gray-500 p-3 cursor-pointer hover:scale-105'>
-                                    <FaInstagram className='w-7 h-7'/>
+                                    <FaInstagram className='w-7 h-7' />
                                 </div>
                                 <div className='rounded-md shadow-lg shadow-gray-500 p-3 cursor-pointer hover:scale-105'>
-                                    <Image src='/assets/resume_icon.png' alt='resumeicon' width='25' height='25' className='w-7 h-7'/>
+                                    <Image src='/assets/resume_icon.png' alt='resumeicon' width='25' height='25' className='w-7 h-7' />
                                 </div>
                             </div>
                         </div>
